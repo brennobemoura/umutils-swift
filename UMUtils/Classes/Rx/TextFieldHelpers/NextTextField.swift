a95e30a41631fc3d3b13f9bb490778fc0907f800
brennoumobi <brenno@umobi.com.br>
2019-12-15 18:59:32 -0300

    Updates for NextTextField

--- UMUtils/Classes/Rx/TextFieldHelpers/NextTextField.swift
+++ UMUtils/Classes/Rx/TextFieldHelpers/NextTextField.swift
@@ -12,36 +12,138 @@ import RxCocoa
 import RxSwift
 
 public class NextTextField {
-    private weak var field: UITextField!
+
+    private weak var prev: NextTextField?
     private var next: NextTextField?
+    private let isOptional: Bool
+
+    private weak var field: UITextField!
     private let disposeBag: DisposeBag = .init()
+    private weak var button: UIButton!
     
-    private init(_ field: UITextField!) {
+    fileprivate init(_ field: UITextField!,_ isOptional: Bool, previous: NextTextField? = nil) {
         self.field = field
+        self.prev = previous
+        self.isOptional = isOptional
+        self.startListening()
     }
     
     @discardableResult
-    public func onNext(_ field: UITextField!) -> NextTextField {
-        self.next = NextTextField(field)
-        
-        self.field?.rx.controlEvent(.editingDidEndOnExit)
-            .subscribe(onNext: { _ in
-                self.next?.becomeFirstResponder()
-            }).disposed(by: disposeBag)
-        
+    public func onNext(_ field: UITextField!, isOptional: Bool = false) -> NextTextField {
+        self.next = NextTextField(field, isOptional, previous: self)
         return self.next!
     }
-    
-    public static func start(_ field: UITextField) -> NextTextField {
-        return NextTextField(nil).onNext(field)
+
+    private func startListening() {
+        self.field?.rx.controlEvent(.editingDidEndOnExit)
+            .subscribe(onNext: { _ in
+                self.nextOrHit()
+            }).disposed(by: self.disposeBag)
     }
     
+    public static func start(_ field: UITextField, isOptional: Bool = false) -> NextTextField {
+        return Root(field, isOptional)
+    }
+
     private func becomeFirstResponder() {
-        if !(self.field.text?.isEmpty ?? false) {
-            self.next?.becomeFirstResponder()
+        if !self.isAvailable {
+            self.nextOrHit()
             return
         }
         
-        self.field.becomeFirstResponder() ? () : self.next?.becomeFirstResponder()
+        self.field.becomeFirstResponder() ? () : self.nextOrHit()
+    }
+
+    @objc private func nextOrHit() {
+        if let next = self.next {
+            next.becomeFirstResponder()
+            return
+        }
+
+        guard !self.hitIfNeeded() else  {
+            return
+        }
+
+        self.root?.nextAvailable?.field?.becomeFirstResponder()
+    }
+
+    private static func root(in ref: NextTextField!) -> NextTextField.Root? {
+        guard let ref = ref else {
+            return nil
+        }
+
+        if let root = ref as? Root {
+            return root
+        }
+
+        return Self.root(in: ref.prev)
+    }
+
+    private var root: NextTextField.Root? {
+        return Self.root(in: self)
+    }
+
+    var isAvailable: Bool {
+        return self.field?.text?.isEmpty ?? false
+    }
+
+    private func hitIfNeeded() -> Bool {
+        guard let root = self.root else {
+            return false
+        }
+
+        let canHit: Bool = {
+            var root: NextTextField? = root
+            while let next = root {
+                if !next.isOptional && (next.isAvailable) {
+                    return false
+                }
+
+                root = next.next
+            }
+
+            return true
+        }()
+
+        if canHit {
+            self.root?.hitButton?.sendActions(for: .touchUpInside)
+            return true
+        }
+
+        return false
+    }
+
+    public func andHit(on button: UIButton!) {
+        self.button = button
+    }
+}
+
+extension NextTextField {
+    class Root: NextTextField {
+        var hitButton: UIButton? {
+            var ref: NextTextField? = self
+            while let next = ref {
+                if let button = next.button {
+                    return button
+                }
+
+                ref = next.next
+            }
+
+            return nil
+        }
+
+        var nextAvailable: NextTextField? {
+            var ref: NextTextField? = self
+            while let next = ref {
+                if next.isAvailable {
+                    return next
+                }
+
+                ref = next.next
+            }
+
+            return nil
+        }
     }
 }
