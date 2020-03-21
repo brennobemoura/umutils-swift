0255ce5b2c13afc28898ef605bdd812a6ae66e1e
brennoumobi <brenno@umobi.com.br>
2020-03-21 20:15:38 -0300

    Fixed image's resize function

--- UMUtils/Classes/Core/Categories/UIImage+Resizing.swift
+++ UMUtils/Classes/Core/Categories/UIImage+Resizing.swift
@@ -39,7 +39,6 @@ public extension UIImage {
     }
     
     func resize(toSize size: CGSize) -> UIImage? {
-        let size = self.size
         
         let widthRatio  = size.width  / self.size.width
         let heightRatio = size.height / self.size.height
@@ -47,9 +46,9 @@ public extension UIImage {
         // Figure out what our orientation is, and use that to form the rectangle
         var newSize: CGSize
         if(widthRatio > heightRatio) {
-            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+            newSize = CGSize(width: self.size.width * heightRatio, height: self.size.height * heightRatio)
         } else {
-            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+            newSize = CGSize(width: self.size.width * widthRatio,  height: self.size.height * widthRatio)
         }
         
         // This is the rect that we've calculated out and this is what is actually used below
